53de5a65ae5edaec7ca79968f8eb08c183182219
Jakub Konka <kubkon@jakubkonka.com>
2018-09-09 09:40:52 +0200

    Add find method to BinarySearchTree

--- src/datastructs/trees/bst.rs
+++ src/datastructs/trees/bst.rs
@@ -54,6 +54,28 @@ where
             }
         }
     }
+
+    pub fn find(&self, value: &T) -> Option<&T> {
+        let mut node = &self.root;
+
+        loop {
+            let unwrapped = node.as_ref()?;
+
+            if unwrapped.value == *value {
+                return Some(&unwrapped.value);
+            }
+
+            if *value < unwrapped.value {
+                node = &unwrapped.left;
+            } else {
+                node = &unwrapped.right;
+            }
+        }
+    }
+
+    pub fn contains(&self, value: &T) -> bool {
+        self.find(value).is_some()
+    }
 }
 
 impl<T> Node<T>
@@ -87,11 +109,11 @@ mod tests {
 
         assert!(tree.root.is_some());
 
-        if let Some(ref root) = tree.root {
-            assert_eq!(root.value, 5);
-            assert!(root.left.is_none());
-            assert!(root.right.is_none());
-        }
+        let root = tree.root.as_ref().unwrap();
+
+        assert_eq!(root.value, 5);
+        assert!(root.left.is_none());
+        assert!(root.right.is_none());
     }
 
     #[test]
@@ -103,48 +125,96 @@ mod tests {
 
         assert!(tree.root.is_some());
 
-        if let Some(ref root) = tree.root {
-            assert_eq!(root.value, 5);
+        let root = tree.root.as_ref().unwrap();
 
-            assert!(root.left.is_some());
+        assert_eq!(root.value, 5);
+        assert!(root.left.is_some());
 
-            if let Some(ref left) = root.left {
-                assert_eq!(left.value, 3);
-                assert!(left.left.is_none());
-                assert!(left.right.is_none());
-            }
+        let left = root.left.as_ref().unwrap();
 
-            assert!(root.right.is_some());
+        assert_eq!(left.value, 3);
+        assert!(left.left.is_none());
+        assert!(left.right.is_none());
 
-            if let Some(ref right) = root.right {
-                assert_eq!(right.value, 6);
-                assert!(right.left.is_none());
-                assert!(right.right.is_none());
-            }
+        assert!(root.right.is_some());
+
+        let right = root.right.as_ref().unwrap();
+
+        assert_eq!(right.value, 6);
+        assert!(right.left.is_none());
+        assert!(right.right.is_none());
+    }
+
+    use std::cmp::Ordering;
+
+    #[derive(Debug)]
+    struct Test(i32, i32);
+
+    impl PartialEq for Test {
+        fn eq(&self, other: &Test) -> bool {
+            self.0 == other.0
+        }
+    }
+
+    impl PartialOrd for Test {
+        fn partial_cmp(&self, other: &Test) -> Option<Ordering> {
+            self.0.partial_cmp(&other.0)
         }
     }
 
     #[test]
     fn insert_duplicate() {
+        let mut tree = BinarySearchTree::<Test>::new();
+        tree.insert(Test(4, 0));
+
+        {
+            assert!(tree.root.is_some());
+
+            let root = tree.root.as_ref().unwrap();
+
+            assert_eq!(root.value.0, 4);
+            assert_eq!(root.value.1, 0);
+
+            assert!(root.left.is_none());
+            assert!(root.right.is_none());
+        }
+
+        tree.insert(Test(4, 1));
+
+        {
+            assert!(tree.root.is_some());
+
+            let root = tree.root.as_ref().unwrap();
+
+            assert_eq!(root.value.0, 4);
+            assert_eq!(root.value.1, 1);
+
+            assert!(root.left.is_none());
+            assert!(root.right.is_none());
+        }
+    }
+
+    #[test]
+    fn find() {
         let mut tree = BinarySearchTree::<i32>::new();
-        tree.insert(4);
+        tree.insert(3);
+        tree.insert(2);
+        tree.insert(1);
 
-        assert!(tree.root.is_some());
+        assert!(tree.find(&3).is_some());
+        assert_eq!(tree.find(&3).unwrap(), &3);
 
-        if let Some(ref root) = tree.root {
-            assert_eq!(root.value, 4);
-            assert!(root.left.is_none());
-            assert!(root.right.is_none());
-        }
+        assert!(tree.find(&2).is_some());
+        assert_eq!(tree.find(&2).unwrap(), &2);
 
-        tree.insert(4);
+        assert!(tree.find(&1).is_some());
+        assert_eq!(tree.find(&1).unwrap(), &1);
 
-        assert!(tree.root.is_some());
+        assert!(tree.find(&11).is_none());
 
-        if let Some(ref root) = tree.root {
-            assert_eq!(root.value, 4);
-            assert!(root.left.is_none());
-            assert!(root.right.is_none());
-        }
+        tree.insert(11);
+
+        assert!(tree.find(&11).is_some());
+        assert_eq!(tree.find(&11).unwrap(), &11);
     }
 }
